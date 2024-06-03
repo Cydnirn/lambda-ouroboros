64b8a1a01e0bd7c8014920c896d7f9359350219d
Cydnirn <seno@siotics.org>
2024-06-03 16:27:58 +0700

    feat: Update inference

--- iotDemo/inference.py
+++ iotDemo/inference.py
@@ -1,26 +1,26 @@
 # inference.py
 import joblib
 import os
-import json
 import pandas as pd
-from io import StringIO
 
 def model_fn(model_dir):
     model = joblib.load(os.path.join(model_dir, "model.joblib"))
     return model
 
 def input_fn(request_body, request_content_type):
-    print("RequestBody", request_body)
-    try:
-        data = pd.read_json(StringIO(request_body), typ="dictionary")
-        if isinstance(data, pd.Series):
-            data = data.to_frame().T
-            data = pd.get_dummies(data, columns=["location"])
-            data.head()
-            return data
-    except ValueError as e:
-        print("Error in parsing JSON:", e)
-        raise ValueError("Error in parsing JSON: {}".format(e))
+    if request_content_type == 'application/json':
+        try:
+            data = pd.read_json(StringIO(request_body), typ="dictionary")
+            if isinstance(data, pd.Series):
+                data = data.to_frame().T
+                data = pd.get_dummies(data, columns=["location"])
+                data.head()
+                return data
+        except ValueError as e:
+            print("Error in parsing JSON:", e)
+            raise ValueError("Error in parsing JSON: {}".format(e))
+    else:        
+        raise ValueError("Unsupported content type: {}".format(request_content_type))
 
 
 def predict_fn(input_data, model):
